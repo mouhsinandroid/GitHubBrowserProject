5b8892cd303f59cf28d9e49f0d16d5c9b1b64de7
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-08-20 00:19:31 +0200

    Initial commit

---
+++ app/src/main/java/com/mouhsinbr/android/githubbrowserproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mouhsinbr.android.githubbrowserproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
