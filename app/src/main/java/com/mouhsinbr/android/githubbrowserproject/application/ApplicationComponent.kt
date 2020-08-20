7502bc65a8fc52ef5596a14aa286dadd8501ec3d
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-08-21 01:42:15 +0200

    chore : configuration project feat: - create Application package       - create gitHubApi library       - create repository library       - create home library

--- app/src/main/java/com/mouhsinbr/android/githubbrowserproject/application/ApplicationComponent.kt
+++ app/src/main/java/com/mouhsinbr/android/githubbrowserproject/application/ApplicationComponent.kt
@@ -3,6 +3,7 @@ package com.mouhsinbr.android.githubbrowserproject.application
 import android.content.Context
 import com.mouhsinbr.android.githubbrowserproject.githubapi.GitHubApi
 import com.mouhsinbr.android.githubbrowserproject.githubapi.GitHubApiModel
+import com.mouhsinbr.android.repository.AppRepository
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -10,6 +11,8 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [GitHubApiModel::class])
 interface ApplicationComponent {
+
+    fun appRepository(): AppRepository
 
     @Component.Factory
     interface Factory {
